d03a38c6e0abb0e3e807b95c12d74e0a3dd2ff4a
rohanMK <rohankhot511@gmail.com>
2024-04-02 19:25:27 +0530

    Adding program12 to the Github on 2nd April 2024

---
+++ Prog12_ReverseAString.java
@@ -0,0 +1,31 @@
+package javaPrograms;
+
+public class Prog12_ReverseAString {
+	
+	public static void main(String[] args) {
+		
+		// Using StringBuffer
+		/*
+		String str = "I am Working in TCS";
+		
+		StringBuffer buffer = new StringBuffer(str);
+		buffer.reverse();
+		System.out.println(buffer);
+		*/
+		
+		//Using Normal conditional statement
+		//Initialize the String
+		 String str = "I Am Working In TCS", rstr="";
+		 
+		 //Iterate each char through the length of the string
+		 for(int i = 0 ; i < str.length() ; i++) {
+			 //Extract the each char
+			 char ch = str.charAt(i);
+			 //add the each char in front of the existing char
+			 rstr = ch + rstr;
+		 }
+		 System.out.println(rstr);
+		 		
+	}
+
+}
